d4efaf632af3ad325ef4cf52b48d691a23e0cdb7
pinkjagnie <olga.krzysztofik@gmail.com>
2023-11-21 15:51:35 +0100

    Added assets

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import defaultUserProfilePhoto from "./default_user-1000-1000.png";
+import astronautOnRocket from "./astronaut_600-600.png";
+import rocketRide from "./rocketride-400-400.png";
+
+export { defaultUserProfilePhoto, astronautOnRocket, rocketRide };
