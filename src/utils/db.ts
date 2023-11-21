c15532ed639a1014e42741ed2c43e2a69e68a010
pinkjagnie <olga.krzysztofik@gmail.com>
2023-11-21 16:26:23 +0100

    Set up connection with db

---
+++ src/utils/db.ts
@@ -0,0 +1,56 @@
+import _mongoose, { connect } from "mongoose";
+
+declare global {
+  var mongoose: {
+    promise: ReturnType<typeof connect> | null;
+    conn: typeof _mongoose | null;
+  };
+}
+
+if (!process.env.MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections from growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  console.log(mongoose);
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = connect(process.env.MONGODB_URI!, opts).then(
+      (mongoose) => {
+        return mongoose;
+      }
+    );
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default dbConnect;
