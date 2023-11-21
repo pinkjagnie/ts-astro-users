e883617c7e021bb0eebdd6ebd5b9c08ae1607611
pinkjagnie <olga.krzysztofik@gmail.com>
2023-11-21 15:52:50 +0100

    Added Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-100 text-slate-900">
-      <h1>astro users</h1>
+      <Hero />
     </main>
   );
 }
