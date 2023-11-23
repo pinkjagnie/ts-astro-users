9c98c3f4feabb4ec7583fff1e278a6cd1f235dcf
pinkjagnie <olga.krzysztofik@gmail.com>
2023-11-23 14:30:03 +0100

    Fixed issue with hot reload in fetching from db

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,27 +2,42 @@
 
 import { useEffect, useState } from "react";
 
-const Hero = () => {
-  const getUsers = async () => {
-    try {
-      const response = await fetch("/api/get", {
-        headers: {
-          Accept: "application/json",
-          method: "GET",
-        },
-      });
+import { TUser } from "@/types/types";
 
-      if (response) {
-        const data = response.json();
-        console.log(data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+import SingleUserCard from "./SingleUserCard";
+
+const Hero = () => {
+  const [astroUsers, setAstroUsers] = useState<TUser[]>([]);
 
   useEffect(() => {
-    getUsers();
+    fetch("/api/get", {
+      headers: {
+        Accept: "application/json",
+        method: "GET",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAstroUsers(data);
+      });
+
+    // try {
+    //   const response = fetch("/api/get", {
+    //     headers: {
+    //       Accept: "application/json",
+    //       method: "GET",
+    //     },
+    //   });
+
+    //   if (response.ok) {
+    //     const data = await response.json();
+    //     console.log(data);
+    //     setAstroUsers(data);
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }, []);
 
   return (
@@ -32,7 +47,19 @@ const Hero = () => {
           Welcome to outer space where you can find other astronauts!
         </h1>
         <div className="md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-8 md:justify-items-center md:py-4 max-w-[1280px]">
-          cards of astro users - TBD
+          {astroUsers.length > 0 &&
+            astroUsers.map((user) => {
+              return (
+                <>
+                  <h1>{user.firstName}</h1>
+                  <SingleUserCard
+                    key={user._id.toString()}
+                    userId={user._id}
+                    user={user}
+                  />
+                </>
+              );
+            })}
         </div>
       </div>
     </section>
